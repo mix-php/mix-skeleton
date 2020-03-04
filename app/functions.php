c38b1a5cd16c9cba3ef38f1004fac7a0d106c9fb
LIU JIAN <onanying@126.com>
2020-03-04 17:34:34 +0800

    feat: 移除全局方法

--- app/functions.php
+++ app/functions.php
@@ -13,25 +13,3 @@ function config()
 {
     return $GLOBALS['config'];
 }
-
-/**
- * 获取全局App对象
- * @return \Mix\Console\Application
- */
-if (!function_exists('app')) {
-    function app()
-    {
-        return $GLOBALS['app'];
-    }
-}
-
-/**
- * 获取全局上下文对象
- * @return \Mix\Bean\ApplicationContext
- */
-if (!function_exists('context')) {
-    function context()
-    {
-        return app()->context;
-    }
-}
